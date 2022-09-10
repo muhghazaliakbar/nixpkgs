6e114f035611aa0b73cc1160e9ae3b7f0d45fd76
Muh Ghazali Akbar <muhghazaliakbar@icloud.com>
2022-09-10 18:48:16 +0800

    feat: add homebrew

---
+++ system/darwin/homebrew.nix
@@ -0,0 +1,83 @@
+{ config, lib, ... }:
+
+let
+  inherit (lib) mkIf;
+  mkIfCaskPresent = cask: mkIf (lib.any (x: x.name == cask) config.homebrew.casks);
+  brewEnabled = config.homebrew.enable;
+in
+
+{
+  environment.shellInit = mkIf brewEnabled ''
+    eval "$(${config.homebrew.brewPrefix}/brew shellenv)"
+  '';
+
+  # https://docs.brew.sh/Shell-Completion#configuring-completions-in-fish
+  # For some reason if the Fish completions are added at the end of `fish_complete_path` they don't
+  # seem to work, but they do work if added at the start.
+  programs.fish.interactiveShellInit = mkIf brewEnabled ''
+    if test -d (brew --prefix)"/share/fish/completions"
+      set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+      set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
+  '';
+
+  homebrew.enable = true;
+  homebrew.onActivation.cleanup = "zap";
+  homebrew.global.brewfile = true;
+
+  homebrew.taps = [
+    "homebrew/cask"
+    "homebrew/cask-drivers"
+    "homebrew/cask-fonts"
+    "homebrew/cask-versions"
+    "homebrew/core"
+    "homebrew/services"
+    "nrlquaker/createzap"
+    "elastic/tap" # Elasticsearch
+  ];
+
+  # Prefer installing application from the Mac App Store
+  #
+  # Commented apps suffer continual update issue:
+  # https://github.com/malob/nixpkgs/issues/9
+  homebrew.masApps = {
+    "1Password for Safari" = 1569813296;
+    Keynote = 409183694;
+    Numbers = 409203825;
+    Pages = 409201541;
+    "Spark – Email App by Readdle" = 1176895641;
+    "WhatsApp Desktop" = 1147396723;
+    # TODO: Re-enable once macOS Ventura officially launches
+    # Xcode = 497799835;
+  };
+
+  # If an app isn't available in the Mac App Store, or the version in the App Store has
+  # limitiations, e.g., Transmit, install the Homebrew Cask.
+  homebrew.casks = [
+    "1password"
+    "cleanmymac"
+    "firefox"
+    "google-chrome"
+    "postman"
+    "protonvpn"
+    "raycast"
+    "free-download-manager"
+    "visual-studio-code"
+    "vlc"
+    "ray"
+    "asana"
+  ];
+
+  # Configuration related to casks
+  environment.variables.SSH_AUTH_SOCK = mkIfCaskPresent "1password-cli"
+    "/Users/${config.users.primaryUser.username}/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+
+  # For cli packages that aren't currently available for macOS in `nixpkgs`.Packages should be
+  # installed in `../home/default.nix` whenever possible.
+  homebrew.brews = [
+    "swift-format"
+    "swiftlint"
+  ];
+}
